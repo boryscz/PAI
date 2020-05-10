38f7bccd28691c569da6b301c5cc8462d907a1eb
boryscz <57436049+boryscz@users.noreply.github.com>
2020-05-10 11:23:25 +0200

    Lab. 8
    Lab. 8

--- lab7/Model/Employee.php
+++ lab7/Model/Employee.php
@@ -1,6 +1,7 @@
 <?php
  class Employee extends AppModel {
     var $name = 'Employee';
-    var $validate = array('nazwisko' => array('rule' => 'notBlank'),'etat' => array('rule' => 'notBlank'));
+    var $validate = array('nazwisko' => array('rule' => 'notBlank'),'etat' => array('rule' => 'notBlank'), 'placa_pod' => array('rule' => ('greaterThanOrEqual', 0)), 
+    'placa_pod' => array('rule' => ('lessThanOrEqual', 2000)));
  }
 ?>
